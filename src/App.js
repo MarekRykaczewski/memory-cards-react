edd11f61c745ad516facdcd33cee11785c7a4a87
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-08 20:34:27 +0100

    add state to track score

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Card from './components/Card';
 function App() {
 
   const [cards, setCards] = React.useState([])
+  const [score, setScore] = React.useState(0)
 
   React.useEffect(() => {
     setCards(shuffle(allNewCards()))
