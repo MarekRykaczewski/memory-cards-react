ffeefa86bdb3835873762418b6df88ed70fe3b35
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-08 16:49:45 +0100

    Create and render Card.js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import React from 'react';
+import Card from './components/Card';
 
 function App() {
+
+  const [cards, setCards] = React.useState(allNewCards())
+    
+  function allNewCards() {
+      const newCards = []
+      for (let i = 0; i < 12; i++) {
+          newCards.push(i)
+      }
+      return newCards
+  }
+  
+  const cardElements = cards.map(function(card, index) {
+    return(
+        <Card key={index} value={cards[card]}/>
+    )
+})
+
   return (
     <div className="App">
       <header className="App-header">
-
+        {cardElements}
       </header>
     </div>
   );
