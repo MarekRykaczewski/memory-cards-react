472e2898f483f5264217b81779c2e315c3dddec6
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-09 18:17:33 +0100

    remove styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,6 @@ import React from "react"
 import '../App.css';
 
 export default function Card(props) {
-
-    const styles = {
-        // height: "100%",
-        // width: "100%",
-        // backgroundSize: "cover"
-
-    }
-
     return (
         <div className="card">
             <img className="card--image" src={props.imgUrl} alt="character" onClick={props.handleClick}></img>
